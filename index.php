c2fe2a8cf78e1b633103ea1144ff9d0823507043
Menno Vlaming <mennovlaming@gmail.com>
2016-12-13 14:18:48 +0100

    versie met tarieven

--- index.php
+++ index.php
@@ -363,6 +363,83 @@ session_start();
     </div>
 </section>
     
+     <!-- TARIEVEN -->
+    <section id="tarieven">
+           <div class="jumbotron">
+            <div class="container">
+                <h1 style="font-size: 60px"> TARIEVEN </h1> <br />
+                <div>
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#Trouwreportages" aria-controls="home" role="tab" data-toggle="tab">Trouwreportages</a></li>
+                        <li role="presentation"><a href="#Portretfotografie" aria-controls="profile" role="tab" data-toggle="tab">Portretfotografie</a></li>
+                        <li role="presentation"><a href="#Familie" aria-controls="messages" role="tab" data-toggle="tab">Familie & gezin</a></li>
+                        <li role="presentation"><a href="#Zwanger" aria-controls="messages" role="tab" data-toggle="tab">Zwangerschapsshoots</a></li>
+                        <li role="presentation"><a href="#Bedrijfsportretten" aria-controls="settings" role="tab" data-toggle="tab">Bedrijfsportretten</a></li>
+                        <li role="presentation"><a href="#Let_op" aria-controls="settings" role="tab" data-toggle="tab">Let op!</a></li>
+                    </ul>
+
+            
+            <br /> <br />
+                            <div class="tab-content">
+                                <div role="tabpanel" class="tab-pane" id="Trouwreportages">
+                                    <h3>Trouwreportages</h3>
+                                    <p>Informeer vrijblijvend naar onze voordelige tarieven voor het
+                                    maken van een trouwreportage.<br /> Wij geven jullie graag advies over de
+                                    diverse mogelijkheden.</p>
+                                </div>
+                                <div role="tabpanel" class="tab-pane" id="Portretfotografie">
+                                    <h3>Portretfotografie</h3>
+                                    <p>In de studio of op locatie 1-5 personen: <br />
+                                    Met 1-5 personen € 75,- ca 1 uur fotografie,
+                                    minimaal 5 nabewerkte foto’s (groeps- en singlefoto’s) in hoge
+                                    resolutie.</p>
+                                </div>
+                                <div role="tabpanel" class="tab-pane" id="Familie">
+                                    <h3>Familie en Gezin</h3>
+                                    Ouders, kinderen en opa & oma € 85,- ca 1 uur fotografie,
+                                    minimaal 5 nabewerkte foto’s (groeps- en singlefoto’s) in hoge resolutie.
+                                </div>
+                                <div role="tabpanel" class="tab-pane" id="Zwanger">
+                                    <h3>Zwangerschapsshoots</h3>
+                                    <p>€ 85,- per uur het aantal foto's is afhankelijk van de duur van de shoot,
+                                        met een minimum van 5 nabewerkte foto's in hoge resolutie.</p>
+                                </div>
+                                <div role="tabpanel" class="tab-pane" id="Bedrijfsportretten">
+                                    <h3>Bedrijfsportretten</h3>
+                                    <p>Bedrijfsportretten op locatie vanaf € 75,-.
+                                        Een portret van al uw medewerkers (bijv. voor in uw brochure of op uw website)
+                                        tot 10 medewerkers voor € 225,-. <br />
+
+                                        - De foto's worden geleverd in hoge resolutie op usb-stick</p>
+
+                                </div>
+                                <div role="tabpanel" class="tab-pane" id="Let_op">
+                                    <h3>Let op!</h3>
+                                    <p>Alle genoemde bedragen zijn exclusief 21% BTW.
+
+                                        Afdrukken van foto’s De foto’s kunnen, tegen meerprijs en in verschillende formaten, via de website van Mambofotografie worden besteld.<br />
+                                        Als je geen afdruk(ken) wilt, kun je voor de download-optie kiezen of voor een usb-stick tegen meerprijs. <br />
+
+                                        - Alle prijzen genoemd bij de bestel/afdruk service zijn inclusief 21% BTW. <br />
+
+                                        - Reiskosten buiten een straal van 25 km van Den Helder bedragen € 0,39/km. <br />
+
+                                        - Opdrachten dienen vooraf per overschrijving a 50% te worden voldaan. <br />
+
+
+
+                                        Voor alle opdrachten zijn de Algemene Voorwaarden van Mambofotografie.nl van toepassing.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+        </div>
+        </section>
+<!-- EINDE TARIEVEN -->
+    
+
 
     <br /> <br />
 <footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-2" style="background-color: rgb(50, 50, 50); padding-top: 1.75rem; padding-bottom: 1.75rem;">
