9c704be6a9a0a3bb8402b1061d5f48937e148865
LAPTOPTEUN\Teun <244258@cursist.rockopnh.nl>
2016-12-14 15:00:36 +0100

    form-to-email update

--- form-to-email.php
+++ form-to-email.php
@@ -1,8 +1,28 @@
 <?php
+// controleren of het formulier is ingevuld
 if(!isset($_POST['submit']))
 {
     echo "error, u moet eerst het formulier invullen!";
 }
 $naam  = $_POST['naam'];
 $email  = $_POST['email'];
-$message  = $_POST['naam'];
+$bericht  = $_POST['bericht'];
+
+// valideren of er iets is ingevuld
+if (empty($naam) || empty($email) || empty($bericht))
+{
+    echo "Vergeet niet uw naam, email of bericht in te vullen!";
+    exit;
+}
+
+$email_from = "info@mambofotografie.nl";
+$email_subject = "Nieuw bericht na invullen formulier";
+$email_body = "U heeft een nieuw bericht van $naam.\n".
+    "email adres: $email.\n".
+    "Bericht: \n $bericht".
+
+    $to ="info@mambofotografie.nl";
+    $headers = "Van email: $email_from \r\n";
+
+// verzend de email
+mail($to,$email_subject,$email_body,$headers);
